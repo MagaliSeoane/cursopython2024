62325f1d0b2c0be33d43fd9e8cca2ce51ae34bca
MagaliSeoane <magalielizabeth.seoane@estudiantes.unahur.edu.ar>
2024-06-04 20:43:34 -0300

    first commit del curso

---
+++ clase_4_junio.py
@@ -0,0 +1,9 @@
+""""Para comentar lo pongo entre triple comillas
+o también puedo seleccionar todo y luego presiono ALT + SHIFT + A"""
+
+print(5+14)
+
+def sumarNumeros(n1,n2):
+    return n1 + n2
+
+print(sumarNumeros(15,20))
